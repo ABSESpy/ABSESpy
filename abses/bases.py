d248ae5014e14f0f60bda7800f01c1b838d7c954
SongshGeo <songshgeo@gmail.com>
2023-08-30 15:13:38 -0500

    refactor(modules): :recycle: refactor notice and observers

--- abses/bases.py
+++ abses/bases.py
@@ -62,58 +62,60 @@ class Creation(metaclass=ABCMeta):
 
 
 class Notice:
+    """通知模块，全局变量会被实时更新给观察者"""
+
+    __glob_vars__: Set[str] = []
+
     def __init__(self, observer: Optional[Observer] = None):
         self.observers: Set[Observer] = set()
-        self._glob_vars: List[str] = []
+        self._glob_vars = set(self.__glob_vars__)
         if observer is not None:
             self.attach(observer)
 
     def __repr__(self) -> str:
-        return f"<{len(self.observers)} are observing: {self.glob_vars}>"
+        return (
+            f"<Noticing {self.glob_vars} to {len(self.observers)} observers>"
+        )
 
     @property
     def glob_vars(self) -> List[str]:
-        return self._glob_vars
+        """全局变量列表，其中的变量自动更新给所有的观察者"""
+        return sorted(self._glob_vars)
 
-    @glob_vars.setter
-    def glob_vars(self, value: "str|List[str]"):
-        self._glob_vars.extend(make_list(value))
+    def add_glob_vars(self, value: Union[str, List[str]]):
+        """添加新的全局变量"""
+        new_vars = set(make_list(value))
+        for var in new_vars:
+            if not hasattr(self, var):
+                raise AttributeError(
+                    f"{var} is not a variable in {self.__class__}."
+                )
+            self._glob_vars.add(var)
         self.notify()
 
-    def attach(self, observer: Observer):
+    def attach(self, observer: Observer) -> None:
+        """添加一个观察者"""
         self.observers.add(observer)
         observer.notification(self)
 
-    def detach(self, observer: Observer):
+    def detach(self, observer: Observer) -> None:
+        """移除一个观察者"""
         self.observers.remove(observer)
 
-    def notify(self, *args: str):
-        if args:
-            for attr in args:
-                val = getattr(self, attr)
-                for observer in self.observers:
-                    setattr(observer, attr, val)
-            logger.info(f"send {args} to {self.observers}")
+    def notify(self) -> None:
+        """将特定变量通知给观察者"""
         for observer in self.observers:
             observer.notification(self)
 
 
 class Observer(metaclass=ABCMeta):
+    """观察者，模型全局变量变化的时候，会自动变化"""
+
     def notification(self, notice: Notice):
-        self.glob_vars = notice.glob_vars
-        for var in self.glob_vars:
+        """每当全局变量变化的时候，收到通知"""
+        for var in notice.glob_vars:
             value = getattr(notice, var)
             setattr(self, var, value)
-
-    @property
-    def glob_vars(self) -> List[str]:
-        return self._glob_vars
-
-    @glob_vars.setter
-    def glob_vars(self, value: Union[Iterable[str], str]) -> None:
-        if not hasattr(self, "_glob_vars"):
-            self._glob_vars = []
-        self._glob_vars.extend(make_list(value))
 
 
 class Mediator:
--- abses/objects.py
+++ abses/objects.py
@@ -7,138 +7,32 @@
 
 from __future__ import annotations
 
-import logging
-from collections import deque
-from typing import (
-    Any,
-    Callable,
-    Deque,
-    Dict,
-    Iterable,
-    List,
-    Optional,
-    Set,
-    Union,
-)
-
-import pandas as pd
-from agentpy import AttrDict
-from agentpy.model import Model
-from agentpy.objects import Object
-from prettytable import PrettyTable
+from typing import TYPE_CHECKING, List, Optional
 
 from abses.time import TimeDriver
-from abses.tools.func import make_list
 
 from .bases import Observer
 from .log import Log
-from .variable import MAXLENGTH, Data, Variable, VariablesRegistry
 
-logger = logging.getLogger(__name__)
+if TYPE_CHECKING:
+    from abses.main import MainModel
 
 
-class BaseObj(Observer, Log, Object):
+class BaseObj(Observer, Log):
+    """
+    基础对象，所有可以被用户自定义的对象都应该继承此类，包括：
+    自然模块，人类模块，子模块，主体。
+    其特点是这些模块可以访问模型的全局变量、时间驱动器，以及有各种自定义变量。
+    """
+
     def __init__(
         self,
-        model: Model,
+        model: MainModel,
         observer: Optional[bool] = True,
         name: Optional[str] = None,
     ):
-        Object.__init__(self, model=model)
         Log.__init__(self, name=name)
-        self._registry: VariablesRegistry = VariablesRegistry(model=model)
-        self._vars_history: Dict[str, Deque[Variable]] = AttrDict()
         self.glob_vars: List[str] = []
-        self._recording: Set[str] = set()
-        self._reporting: Set[str] = set()
+        self.time = TimeDriver(model)
         if observer:
             model.attach(self)
-
-    def __getattr__(self, __name: str) -> Any:
-        # if __name in self.vars:
-        #     now = super().__getattribute__(__name)
-        #     var = self.to_variable(__name, now)
-        #     return var.get_value()
-        return super().__getattribute__(__name)
-
-    @property
-    def vars(self) -> List[str]:
-        return self._registry[self]
-
-    @property
-    def time(self) -> TimeDriver:
-        return self.model.time
-
-    @property
-    def db(self) -> Dict:
-        return self.model.db
-
-    @property
-    def output(self) -> pd.DataFrame:
-        return pd.DataFrame(self.log, index=self.time.time[:-1])
-
-    @property
-    def recording(self) -> Set[str]:
-        return self._recording
-
-    @recording.setter
-    def recording(self, variables: Union[str, Iterable[str]]) -> None:
-        self._recording = self.recording.union(set(make_list(variables)))
-
-    @property
-    def reporting(self) -> Set[str]:
-        return self._reporting
-
-    @reporting.setter
-    def reporting(self, variables: Union[str, Iterable[str]]) -> None:
-        self._reporting = self.reporting.union(make_list(variables))
-
-    def _when_time_go(self):
-        self.record(self._recording)
-        for var in self._registry[self]:
-            value_now = self.__getattr__(var)
-            self._vars_history[var].append(value_now)
-
-    def to_variable(self, name, now: Optional[Data] = None) -> Data:
-        self._registry.check_variable(name, value=now)
-        history = self._vars_history[name]
-        return Variable(owner=self, history=history, now=now)
-
-    def register_a_var(
-        self,
-        name: str,
-        init_data: Optional[Data] = None,
-        long_name: Optional[str] = None,
-        units: Optional[str] = None,
-        check_func: Optional[Iterable[Callable]] = None,
-    ) -> Variable:
-        dtype = type(init_data)
-        self._registry.register(
-            owner=self,
-            var_name=name,
-            long_name=long_name,
-            units=units,
-            dtype=dtype,
-            check_func=check_func,
-        )
-        self._vars_history[name] = deque([], maxlen=MAXLENGTH)
-        self.__setattr__(name, init_data)
-        return self.to_variable(name=name, now=init_data)
-
-    def report(
-        self,
-        max_width: int = 30,
-        decimal: int = 4,
-    ) -> PrettyTable:
-        table = PrettyTable()
-        table.field_names = ["Variable", "Value"]
-        for attr, val in self.__dict__.items():
-            if attr in ("id", "type"):
-                continue
-            if attr.startswith("_"):
-                continue
-            table.add_row([attr, val])
-        table.max_width = max_width
-        table.title = f"{self.type} {self.id}:"
-        table.float_format = f".{decimal}"
-        return table
