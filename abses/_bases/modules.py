eee67d3ad8945ba0be4deb8c10a4b754054c3f6e
SongshGeo <songshgeo@gmail.com>
2024-05-16 10:51:23 +0800

    fix(src): :bug: fixed module_class type incomptible error when "how" kwarg is assigned

--- abses/_bases/modules.py
+++ abses/_bases/modules.py
@@ -23,6 +23,7 @@ from typing import (
     Optional,
     Type,
     TypeVar,
+    cast,
 )
 
 from loguru import logger
@@ -99,7 +100,7 @@ class _ModuleFactory(Generic[ModuleType]):
     """To create a module."""
 
     methods: List[str] = []
-    default_cls: type[Module] = Module
+    default_cls: Type[Module] = Module
 
     def __init__(self, father) -> None:
         self.father: CompositeModule = father
@@ -119,15 +120,15 @@ class _ModuleFactory(Generic[ModuleType]):
 
     def _check_cls(
         self, module_cls: Optional[Type[ModuleType]]
-    ) -> Type[Module]:
+    ) -> Type[ModuleType]:
         """Check if the provided class is a valid module class."""
         if module_cls is None:
-            return self.default_cls
+            return cast(Type[ModuleType], self.default_cls)
         if not issubclass(module_cls, self.default_cls):
             raise TypeError(
                 f"'{module_cls}' not a subclass of {self.default_cls}."
             )
-        return module_cls
+        return cast(Type[ModuleType], module_cls)
 
     @property
     def is_empty(self) -> bool:
@@ -167,11 +168,13 @@ class _ModuleFactory(Generic[ModuleType]):
         Returns:
             The created module.
         """
-        self._check_cls(module_cls=module_class)
+        module_cls = self._check_cls(module_cls=module_class)
         if not how:
-            module = module_class(model=self.father.model, **kwargs)
+            module = module_cls(model=self.father.model, **kwargs)
         elif hasattr(self, how):
-            module = getattr(self, how)(model=self.father.model, **kwargs)
+            module = getattr(self, how)(
+                module_cls=module_cls, model=self.father.model, **kwargs
+            )
         else:
             raise ValueError(
                 f"{how} is not a valid method for creating module."
